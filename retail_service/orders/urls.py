35c7aed4951ca03d692ac811bcc65b33b5325137
uk <mail@mail.ru>
2024-09-09 16:03:50 +0300

    removed some bloat

--- retail_service/orders/urls.py
+++ retail_service/orders/urls.py
@@ -1,17 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-# from .views import ShopViewSet, CategoryViewSet, ProductViewSet, OrderViewSet, UserContactViewSet
 from . import api_views
 
-# router = DefaultRouter()
-# router.register(r'stores', ShopViewSet)
-# router.register(r'categories', CategoryViewSet)
-# router.register(r'products', ProductViewSet)
-# router.register(r'orders', OrderViewSet)
-# router.register(r'usercontacts', UserContactViewSet)
 
 urlpatterns = [
-    # path('', include(router.urls)),
     path('login/', api_views.LoginView.as_view(), name='login'),
     path('register/', api_views.RegisterView.as_view(), name='register'),
     path('products/', api_views.ProductListView.as_view(), name='product-list'),
